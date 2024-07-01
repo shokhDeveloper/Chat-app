a4dd381c93532bbc89c4fb38977d7518f8fd3f06
Shohijahon Musinkulov <110657735+shokhDeveloper@users.noreply.github.com>
2024-07-01 14:59:12 +0500

    Added group update and group delete

--- src/controller/groupController.js
+++ src/controller/groupController.js
@@ -1,4 +1,4 @@
-import { ClientError, globalError } from "#utils/error.js"
+import { ClientError, ServerError, globalError } from "#utils/error.js"
 import { fetch } from "#utils/postgres.js";
 import { groupValidator } from "#utils/validator.js";
 
@@ -15,6 +15,47 @@ export const groupController = {
             return res.status(201).json({message: "Group created successfully", group: insertGroup, statusCode: 201});
         }catch(error){
             return globalError(res, error)
+        }
+    },
+    GET: async function(req, res){
+        try{
+            const {groupId} = req.params;
+            if(groupId) {
+                const group = await fetch(`SELECT * FROM groups WHERE group_id=$1;`, true, groupId);
+                if(group) return res.status(200).json(group)
+                else return res.status(200).json({message: "Group not found", code: 404})
+            }
+            const groups = await fetch(`SELECT * FROM groups`);
+            return res.status(200).json(groups);
+        }catch(error){
+            return globalError(res, error);
+        }
+    },
+    PUT: async function(req, res){
+        try{
+            const {group_name} = req.body;
+            const {groupId} = req.params;
+            if(!groupId) throw new ClientError("Group not found !", 404);
+            const group = await fetch(`SELECT * FROM groups WHERE group_id=$1;`, true, groupId)
+            if(!(req.user_info.user_id == group.user_id)) throw new ClientError("You have an incorrect token to update this group", 401); 
+            const updateGroupRes = await fetch(`UPDATE groups SET group_name=$1 WHERE group_id=$2 RETURNING *`, true, group_name, groupId);
+            if(!updateGroupRes.group_id) throw new ServerError("An error occurred while updating the group !");
+            return res.status(200).json({message: "Group updated successfully", group: updateGroupRes, status: 200})
+        }catch(error){
+            return globalError(res, error);
+        };
+    },
+    DELETE: async function(req, res){
+        try{
+            const {groupId} = req.params;
+            if(!groupId) throw new ClientError("Group not found !", 404);
+            const group = await fetch(`SELECT * FROM groups WHERE group_id=$1;`, true, groupId)
+            if(!(req.user_info.user_id == group.user_id)) throw new ClientError("You have an incorrect token to update this group", 401); 
+            const deleteGroupRes = await fetch(`DELETE from groups WHERE group_id=$1 RETURNING *`, true, groupId);
+            if(!deleteGroupRes.group_id) throw new ServerError("An error occurred while updating the group !");
+            return res.status(200).json({message: "Group deleted successfully", group: deleteGroupRes});
+        }catch(error){
+            return globalError(res, error);
         }
     }
 }
--- src/router/groupRouter.js
+++ src/router/groupRouter.js
@@ -3,3 +3,5 @@ import express from "express";
 export const groupRouter = express.Router();
 
 groupRouter.route("/").post(groupController.POST);
+groupRouter.route("/all").get(groupController.GET);
+groupRouter.route("/:groupId").get(groupController.GET).put(groupController.PUT).delete(groupController.DELETE);
